471536f4ee4ac996a31f7a8f2ebe6e1f7ac62b42
santhosh kumar <santhoshkumar3479@gmail.com>
2024-12-30 10:45:47 +0530

    Initial Commit

---
+++ ChatAppTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppTask1
+//
+//  Created by iPhone Apps Nano Nino on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChatAppTask1Tests/ChatAppTask1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppTask1Tests.swift
+//  ChatAppTask1Tests
+//
+//  Created by iPhone Apps Nano Nino on 30/12/24.
+//
+
+import Testing
+@testable import ChatAppTask1
+
+struct ChatAppTask1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
